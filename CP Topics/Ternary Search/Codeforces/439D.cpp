18388ed9d20e435867b0d8d9d9480f93b82ad035
MD. SIAM AHMED <mdsiamahmed35862@gmail.com>
2026-05-20 22:43:45 -0500

    ternary cf

---
+++ CP Topics/Ternary Search/Codeforces/439D.cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define input(v)       \
+    for (auto &it : v) \
+    cin >> it
+#define inputa(a, n, ind)   \
+    for (i = 0; i < n; i++) \
+    cin >> a[i + ind]
+#define output(v)      \
+    for (auto &it : v) \
+    cout << it << " "
+#define outputa(a, n, ind)  \
+    for (i = 0; i < n; i++) \
+    cout << a[i + ind] << " "
+#define outputl(v)     \
+    for (auto &it : v) \
+    cout << it << '\n'
+#define outputal(a, n, ind) \
+    for (i = 0; i < n; i++) \
+    cout << a[i + ind] << '\n'
+
+#define maxn (int)(2e5 + 100)
+#define M 998244353
+
+template <class T>
+struct comparator
+{
+    bool operator()(const T &a, const T &b) const
+    {
+        return a < b;
+    }
+};
+
+template <class T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <class T>
+using ordered_setd = tree<T, null_type, greater<T>, rb_tree_tag,
+                          tree_order_statistics_node_update>;
+int f(int *a, int *b, int n, int m, int x)
+{
+    int i;
+    int val = 0;
+    for (i = 0; i < n; i++)
+    {
+        val+=max(0LL,x-a[i]);
+    }
+    for (i = 0; i < m; i++)
+    {
+        val+=max(0LL,b[i]-x);
+    }
+
+    return val;
+}
+
+int ternary_search_minimum(int *a, int *b, int n, int m)
+{
+    int l = 1, r = 1e9;
+    if (l == r)
+        return l;
+    else if (f(a,b,n,m, r) < f(a,b,n,m, r - 1))
+        return r;
+    else if (f(a,b,n,m, l) < f(a,b,n,m, l + 1))
+        return l;
+    while ((r - l) > 3)
+    {
+        int m1 = l + (r - l) / 3;
+        int m2 = r - (r - l) / 3;
+
+        if (f(a,b,n,m, m1) > f(a,b,n,m, m2))
+        {
+            l = m1;
+            if (f(a,b,n,m, m2) > f(a,b,n,m, m2 + 1))
+                l = m2;
+            else
+                r = m2;
+        }
+        else if (f(a,b,n,m, m1) == f(a,b,n,m, m2))
+        {
+            l = m1;
+            r = m2;
+        }
+        else
+        {
+            r = m2;
+            if (f(a,b,n,m, m1) > f(a,b,n,m, m1 + 1))
+                l = m1;
+            else
+                r = m1;
+        }
+    }
+    int min_ind = l;
+    for (int i = l + 1; i <= r; i++)
+    {
+        if (f(a,b,n,m, i) < f(a,b,n,m, min_ind))
+        {
+            min_ind = i;
+        }
+    }
+    return min_ind;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+
+    int a[n], b[m];
+    int i, j;
+
+    inputa(a, n, 0);
+    inputa(b, m, 0);
+
+    int min_ind = ternary_search_minimum(a, b, n, m);
+    int ans=f(a,b,n,m,min_ind);
+    cout<<ans<<endl;
+
+    return 0;
+}
