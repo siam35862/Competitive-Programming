eeec206f6a673e1b547f10300682be6fbe1f4d3c
MD. SIAM AHMED <mdsiamahmed35862@gmail.com>
2026-06-25 17:56:59 +0600

    Create 816B.cpp

---
+++ OJ/Codeforces/Contest/Div 2/Codeforces Round 419 (Div. 2)/816B.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define input(v) for(auto &it:v)cin>>it
+#define inputa(a,n,ind) for(i=0;i<n;i++)cin>>a[i+ind]
+#define output(v) for(auto &it:v)cout<<it<<" "
+#define outputa(a,n,ind) for(i=0;i<n;i++)cout<<a[i+ind]<<" "
+#define outputl(v) for(auto &it:v)cout<<it<<'\n'
+#define outputal(a,n,ind) for(i=0;i<n;i++)cout<<a[i+ind]<<'\n'
+
+#define maxn (int)(2e5+100)
+#define M 998244353
+#define inf 1e18
+
+template <class T>
+struct comparator
+{
+    bool operator()(const T &a, const T &b) const
+    {
+        return a < b;
+    }
+};
+
+template <class T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <class T>
+using ordered_setd = tree<T, null_type, greater<T>, rb_tree_tag,
+                          tree_order_statistics_node_update>;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n,k,q;
+    cin>>n>>k>>q;
+    vector<int> diff(maxn+2,0);
+    vector<int> psum(maxn+1,0);
+    for(int i=0;i<n;i++)
+    {
+        int l,r;
+        cin>>l>>r;
+        diff[l]+=1;
+        diff[r+1]-=1;
+
+    }
+    for(int i=1;i<=maxn;i++)
+    {
+        diff[i]+=diff[i-1];
+
+       
+        psum[i]+=psum[i-1];
+        psum[i]+=(diff[i]>=k)?1:0;
+    }
+    for(int i=0;i<q;i++)
+    {
+        int l,r;
+        cin>>l>>r;
+        cout<<psum[r]-psum[l-1]<<endl;
+    }
+    
+    return 0;
+}
