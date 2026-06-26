5c946ecf637eed350e15a062d9229576ff4329a6
MD. SIAM AHMED <mdsiamahmed35862@gmail.com>
2026-06-27 00:15:48 +0600

    Create 1638-Grid-Paths-I.cpp

---
+++ CP Topics/Dynamic Programming/0 1 Knapsack/CSES/1638-Grid-Paths-I.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define input(v)       \
+    for (auto &it : v) \
+    cin >> it
+#define inputa(a, n, ind)   \
+    for (i = 0; i < n; i++) \
+    cin >> a[i + ind]
+#define output(v)      \
+    for (auto &it : v) \
+    cout << it << " "
+#define outputa(a, n, ind)  \
+    for (i = 0; i < n; i++) \
+    cout << a[i + ind] << " "
+#define outputl(v)     \
+    for (auto &it : v) \
+    cout << it << '\n'
+#define outputal(a, n, ind) \
+    for (i = 0; i < n; i++) \
+    cout << a[i + ind] << '\n'
+
+#define maxn (int)(2e5 + 100)
+#define M 998244353
+#define inf 1e18
+
+template <class T>
+struct comparator
+{
+    bool operator()(const T &a, const T &b) const
+    {
+        return a < b;
+    }
+};
+
+template <class T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <class T>
+using ordered_setd = tree<T, null_type, greater<T>, rb_tree_tag,
+                          tree_order_statistics_node_update>;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int dp[n][n] = {};
+    string s[n];
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+
+        cin >> s[i];
+        for (j = 0; j < n; j++)
+        {
+            if (s[i][j] == '*')
+                dp[i][j] = 0;
+            else
+            {
+                if(i==0&&j==0)dp[i][j]=1;
+                if (i > 0)
+                    dp[i][j] += dp[i - 1][j];
+                if (j > 0)
+                    dp[i][j] += dp[i][j - 1];
+            }
+        }
+    }
+    cout<<dp[n-1][n-1]<<endl;
+
+    return 0;
+}
