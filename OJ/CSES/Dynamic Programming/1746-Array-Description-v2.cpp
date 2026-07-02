52961ed81532be4b3c9237c463d0bfc8c204195e
MD. SIAM AHMED <mdsiamahmed35862@gmail.com>
2026-07-02 09:39:06 +0600

    Create 1746-Array-Description-v2.cpp

---
+++ OJ/CSES/Dynamic Programming/1746-Array-Description-v2.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define input(v)       \
+    for (auto &it : v) \
+    cin >> it
+#define inputa(a, n, ind)       \
+    for (int i = 0; i < n; i++) \
+    cin >> a[i + ind]
+#define output(v)      \
+    for (auto &it : v) \
+    cout << it << " "
+#define outputa(a, n, ind)      \
+    for (int i = 0; i < n; i++) \
+    cout << a[i + ind] << " "
+#define outputl(v)     \
+    for (auto &it : v) \
+    cout << it << '\n'
+#define outputal(a, n, ind)     \
+    for (int i = 0; i < n; i++) \
+    cout << a[i + ind] << '\n'
+
+#define maxn (int)(2e5 + 100)
+#define M (int)1e9 + 7
+#define inf 1e18
+
+template <class T>
+struct comparator
+{
+    bool operator()(const T &a, const T &b) const
+    {
+        return a < b;
+    }
+};
+
+template <class T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <class T>
+using ordered_setd = tree<T, null_type, greater<T>, rb_tree_tag,
+                          tree_order_statistics_node_update>;
+
+vector<vector<int>> dp;
+int th;
+int num_of_ways(int n, int m, int *a)
+{
+    if (n < 0 || m < 1 || m > th)
+        return 0;
+    if (dp[n][m] != -1)
+        return dp[n][m];
+    if (a[n] != 0 && a[n] != m)
+        return dp[n][m] = 0;
+    if (n == 0)
+    {
+        return dp[n][m] = 1;
+    }
+    int value = 0;
+
+    value += num_of_ways(n - 1, m - 1, a);
+    value += num_of_ways(n - 1, m, a);
+    value += num_of_ways(n - 1, m + 1, a);
+    value %= M;
+    return dp[n][m] = value;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+    th = m;
+    int a[n];
+    inputa(a, n, 0);
+    dp.resize(n, vector<int>(m + 1, -1));
+    int ans = 0;
+    for (int i = 1; i <= m; i++)
+    {
+        ans += num_of_ways(n - 1, i, a);
+        ans %= M;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
