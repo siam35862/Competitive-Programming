bbe360fff415607902d5e214c22f60ebd81dd528
MD. SIAM AHMED <mdsiamahmed35862@gmail.com>
2026-07-02 09:21:15 +0600

    Update 1746-Array-Description.cpp

--- CP Topics/Dynamic Programming/Basic DP/CSES/1746-Array-Description.cpp
+++ CP Topics/Dynamic Programming/Basic DP/CSES/1746-Array-Description.cpp
@@ -30,7 +30,7 @@ using namespace std;
     cout << a[i + ind] << '\n'
 
 #define maxn (int)(2e5 + 100)
-#define M 998244353
+#define M (int)1e9+7
 #define inf 1e18
 
 template <class T>
@@ -86,6 +86,7 @@ int32_t main()
                     if (j <= m && j >= 1)
                     {
                         dp[i][a[i]] += dp[i-1][j];
+                        dp[i][a[i]]%=M;
                     }
                 }
             }
@@ -98,6 +99,7 @@ int32_t main()
                         if (k >= 1 && k <= m)
                         {
                             dp[i][j] += dp[i-1][k];
+                            dp[i][j]%=M;
                         }
                     }
                 }
@@ -110,6 +112,7 @@ int32_t main()
         for(i=1;i<=m;i++)
         {
             ans+=dp[n-1][i];
+            ans%=M;
         }
     }
     else ans=dp[n-1][a[n-1]];
