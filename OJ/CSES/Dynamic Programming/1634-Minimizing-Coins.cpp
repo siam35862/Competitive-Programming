59bf3855060d64d73ed2e92d142eb2bb0bf3d20c
MD. SIAM AHMED <mdsiamahmed35862@gmail.com>
2026-07-02 06:56:53 +0600

    Create 1634-Minimizing-Coins.cpp

---
+++ OJ/CSES/Dynamic Programming/1634-Minimizing-Coins.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define input(v)       \
+    for (auto &it : v) \
+    cin >> it
+#define inputa(a, n, ind)       \
+    for (int i = 0; i < n; i++) \
+    cin >> a[i + ind]
+#define output(v)      \
+    for (auto &it : v) \
+    cout << it << " "
+#define outputa(a, n, ind)      \
+    for (int i = 0; i < n; i++) \
+    cout << a[i + ind] << " "
+#define outputl(v)     \
+    for (auto &it : v) \
+    cout << it << '\n'
+#define outputal(a, n, ind)     \
+    for (int i = 0; i < n; i++) \
+    cout << a[i + ind] << '\n'
+
+#define maxn (int)(2e5 + 100)
+#define M 998244353
+#define inf 1e18
+
+template <class T>
+struct comparator
+{
+    bool operator()(const T &a, const T &b) const
+    {
+        return a < b;
+    }
+};
+
+template <class T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <class T>
+using ordered_setd = tree<T, null_type, greater<T>, rb_tree_tag,
+                          tree_order_statistics_node_update>;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    inputa(a, n, 0);
+    vector<int> dp(k + 1, inf);
+    dp[0] = 0;
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 1; j <= k; j++)
+        {
+            if (j - a[i] >= 0)
+                dp[j] = min(dp[j], dp[j-a[i]]+1);
+        }
+    }
+   
+    if(dp[k]==inf)cout<<-1<<endl;
+    else cout<<dp[k]<<endl;
+
+    return 0;
+}
