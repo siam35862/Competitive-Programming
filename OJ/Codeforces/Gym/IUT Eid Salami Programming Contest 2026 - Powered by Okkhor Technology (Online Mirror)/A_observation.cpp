7fd70d2214f7c8d94161aaa0c628305098282be2
MD. SIAM AHMED <mdsiamahmed35862@gmail.com>
2026-08-04 23:42:48 +0600

    Create A_observation.cpp

---
+++ OJ/Codeforces/Gym/IUT Eid Salami Programming Contest 2026 - Powered by Okkhor Technology (Online Mirror)/A_observation.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define input(v)       \
+    for (auto &it : v) \
+    cin >> it
+#define inputa(a, n, ind)       \
+    for (int i = 0; i < n; i++) \
+    cin >> a[i + ind]
+#define output(v)      \
+    for (auto &it : v) \
+    cout << it << " "
+#define outputa(a, n, ind)      \
+    for (int i = 0; i < n; i++) \
+    cout << a[i + ind] << " "
+#define outputl(v)     \
+    for (auto &it : v) \
+    cout << it << '\n'
+#define outputal(a, n, ind)     \
+    for (int i = 0; i < n; i++) \
+    cout << a[i + ind] << '\n'
+
+#define maxn (int)(2e5 + 100)
+#define M 998244353
+#define inf 1e18
+
+template <class T>
+struct comparator
+{
+    bool operator()(const T &a, const T &b) const
+    {
+        return a < b;
+    }
+};
+
+template <class T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <class T>
+using ordered_setd = tree<T, null_type, greater<T>, rb_tree_tag,
+                          tree_order_statistics_node_update>;
+int f(int n)
+{
+    int m = 0;
+    while (n > 0)
+    {
+        m += n % 10;
+        n /= 10;
+    }
+    return m;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    int v[n + 1] = {};
+
+    int i;
+    for (i = 1; i <= n; i++)
+    {
+        int temp = i * i;
+        while (temp >= 10)
+        {
+            temp = f(temp);
+        }
+        v[i] = temp;
+        v[i] += v[i - 1];
+
+        if (i % 10 == 0)
+            cout << i - 10 << " " << i << " " << v[i] - v[i - 10] << endl;
+        // if (i % 100 == 0)
+        //     cout<<endl << i - 100 << " " << i << " " << v[i] - v[i - 100] << endl;
+    }
+
+    return 0;
+}
